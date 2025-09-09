0962e306fc6af43cca5b1e8807b7e67e967b5c79
Nitin Malviya <nitinmalviya172@gmail.com>
2025-09-09 23:15:15 +0530

    update the data

--- one/src/App.js
+++ one/src/App.js
@@ -1,9 +1,21 @@
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Shop from "./pages/Shop";
+import Navbar from "./components/Navar";
+import Contact from "./pages/Contact";
+import About from "./pages/About";
 
 function App() {
   return (
-    <div className="App">
-      home
-    </div>
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/shop" element={<Shop />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </>
   );
 }
 
